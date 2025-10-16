0c1afd3521630b467f49c172661ba2cad17ec145
joshinvibin <157175863+joshinvibin@users.noreply.github.com>
2025-10-16 21:54:27 +0100

    added some bits to prep adding matching letters to frequency

---
+++ frequencyCount/Module1.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Console
+Module Module1
+
+    Function Freq_Analysis(word As String) As Integer()
+        Dim letters(26) As Integer
+        Dim letter As Integer = 0
+
+        For i = 0 To Len(word) - 1
+            If Asc(word(i)) < 92 And Asc(word(i)) > 64 Then
+                letter = Asc(word(i)) - 64
+
+                letters(letter) += 1
+            End If
+        Next i
+        WriteLine()
+        For i = 1 To 26
+            Write(Chr(i + 64) & ": " & letters(i) & "    ")
+        Next
+
+        Return letters
+    End Function
+    Sub Main()
+        Dim word As String = ReadLine()
+        Dim letters(26) As Integer
+        Dim freqTab(26) As String
+
+        Dim temp As Integer = 0
+        letters = Freq_Analysis(word)
+
+        'For i = 1 To 25
+        '    If letters(i) > letters(i) + 1 Then
+        '        temp = letters(i + i)
+        '        letters(i + 1) = letters(i)
+        '        letters(i) = temp
+        '    End If
+        'Next
+    End Sub
+
+End Module
---
+++ replacement/Module1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim text As String = ReadLine()
+        Dim text1 As String = ""
+        Dim encrypt As String = ""
+        Dim decrypt As String = ""
+
+        Do
+            encrypt = ReadLine()
+            decrypt = ReadLine()
+
+
+            For i = 0 To text.Length() - 1
+                If text(i) = encrypt Then
+                    text1 += decrypt
+                Else
+                    text1 += text(i)
+                End If
+            Next
+
+            WriteLine(text)
+        Loop
+    End Sub
+
+End Module
