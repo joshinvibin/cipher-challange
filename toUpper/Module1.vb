cd5e5b9d184dbfdcb631bbc39851c5dae764a153
joshinvibin <157175863+joshinvibin@users.noreply.github.com>
2025-10-17 09:49:03 +0100

    add toUpper

---
+++ toUpper/Module1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim text As String = Console.ReadLine()
+
+        WriteLine()
+        WriteLine(text.ToUpper())
+    End Sub
+
+End Module
