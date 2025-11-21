709e7ca6ae0980a7477d027eb1e2c2f20988326b
joshinvibin <157175863+joshinvibin@users.noreply.github.com>
2025-11-21 22:40:10 +0000

    add vigenere and dirty vigenere

---
+++ VigenereCipher/Module1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Vingenere(ByRef word As String)
+        Dim word1 As String = ""
+        Dim letter As Integer = 0
+        Dim key As String = ""
+        Dim count As Integer = 0
+        key = ReadLine().ToUpper()
+        For i = 0 To Len(word) - 1
+            If word(i) <> " " Then
+                letter = Asc(word(i))
+                letter -= (Asc(key(count)) - 65)
+                count = count + 1
+                If count = 4 Then count = 0
+            Else
+                letter = 32
+            End If
+
+            If letter > 90 Then
+                letter = 65 + (letter Mod 26)
+            ElseIf letter < 65 And letter <> 32 Then
+                letter = 91 - (letter Mod 26)
+            End If
+
+            word1 += Chr(letter)
+        Next
+
+        WriteLine(word1)
+    End Sub
+
+    Sub Main()
+        Dim word As String = ReadLine()
+        Vingenere(word)
+    End Sub
+
+End Module
---
+++ Vigerenre2/Module1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim word As String = ReadLine()
+        Dim letterasc As Integer = 0
+        Dim decrypt As String = ""
+        Dim count As Integer = 1
+        WriteLine()
+        WriteLine()
+        For i = 0 To word.Length() - 1
+            If word(i) <> " " Then
+                Select Case count
+                    Case 1
+                        letterasc = Asc(word(i)) - 13
+                    Case 2
+                        letterasc = Asc(word(i)) - 4
+                    Case 3
+                        letterasc = Asc(word(i)) - 22
+                    Case 4
+                        letterasc = Asc(word(i)) - 24
+                    Case 5
+                        letterasc = Asc(word(i)) - 14
+                    Case 6
+                        letterasc = Asc(word(i)) - 17
+                    Case 7
+                        letterasc = Asc(word(i)) - 10
+                End Select
+                count = count + 1
+                If count = 8 Then count = 1
+                If letterasc < 65 Then letterasc += 26
+
+                decrypt += Chr(letterasc)
+            Else
+                decrypt += " "
+            End If
+        Next i
+
+        WriteLine(decrypt)
+    End Sub
+
+End Module
