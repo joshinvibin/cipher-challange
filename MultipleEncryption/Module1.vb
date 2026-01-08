71b63aefa6c4e03666fca1444fc1ff2d81db96ba
joshinvibin <157175863+joshinvibin@users.noreply.github.com>
2026-01-08 20:46:22 +0000

    added vernam to multiple encrypt

--- MultipleEncryption/Module1.vb
+++ MultipleEncryption/Module1.vb
@@ -2,8 +2,8 @@
 Module Module1
 
     Function tryCatch(message As String) As Integer
-        Dim crash As Boolean
-        Dim input As Integer
+        Dim crash As Boolean = False
+        Dim input As Integer = 0
 
         Do
             WriteLine($"{message}")
@@ -47,8 +47,8 @@ Module Module1
     End Function
 
     Sub encrypt()
-        Dim input As Integer = 0
-        input = userChoice(" _____                             _
+        Dim output As String = ""
+        Dim input As Integer = userChoice(" _____                             _
 | ____|_ __   ___ _ __ _   _ _ __ | |_
 |  _| | '_ \ / __| '__| | | | '_ \| __|
 | |___| | | | (__| |  | |_| | |_) | |_
@@ -63,18 +63,22 @@ Please choose an option:
 
         Select Case input
             Case 0
-                caesar(1)
+                output = caesar(1)
             Case 1
-
+                output = vernam(1)
             Case 2
-                vigenere(1)
+                output = vigenere(1)
             Case 3
-                encryptRailFence()
+                output = encryptRailFence()
         End Select
+
+        WriteLine("Your encrypted text is :")
+        WriteLine(output)
         ReadLine()
     End Sub
 
     Sub decrypt()
+        Dim output As String = ""
         Dim input As Integer = userChoice(" ____                             _
 |  _ \  ___  ___ _ __ _   _ _ __ | |_
 | | | |/ _ \/ __| '__| | | | '_ \| __|
@@ -92,23 +96,142 @@ Please choose an option:
 
         Select Case input
             Case 0
-                caesar(-1)
+                output = caesar(-1)
             Case 1
-
+                output = vernam(-1)
             Case 2
-                vigenere(-1)
+                output = vigenere(-1)
             Case 3
-                decryptRailFence()
+                output = decryptRailFence()
         End Select
+
+        WriteLine("Your decrypted text is :")
+        WriteLine(output)
+
         ReadLine()
     End Sub
 
-    Sub decryptRailFence()
+    Function vernam(direction As Integer) As String
+        Dim type As String = "plain"
+        Dim key As String = ""
+        Dim output As String = ""
         Dim input As String = ""
+        If direction = -1 Then type = "encrypted"
+        WriteLine($"__     __                              
+\ \   / /__ _ __ _ __   __ _ _ __ ___  
+ \ \ / / _ \ '__| '_ \ / _` | '_ ` _ \ 
+  \ V /  __/ |  | | | | (_| | | | | | |
+   \_/ \___|_|  |_| |_|\__,_|_| |_| |_|
+
+Enter you {type} text")
+
+        input = ReadLine()
+
+        Clear()
+
+        Select Case direction
+            Case 1
+                Dim choiceIn As Integer = userChoice("__     __                              
+\ \   / /__ _ __ _ __   __ _ _ __ ___  
+ \ \ / / _ \ '__| '_ \ / _` | '_ ` _ \ 
+  \ V /  __/ |  | | | | (_| | | | | | |
+   \_/ \___|_|  |_| |_|\__,_|_| |_| |_|
+
+Do you have a key?
+    Yes
+    No", 7, 1)
+                Select Case choiceIn
+                    Case 0
+                        WriteLine("__     __                              
+\ \   / /__ _ __ _ __   __ _ _ __ ___  
+ \ \ / / _ \ '__| '_ \ / _` | '_ ` _ \ 
+  \ V /  __/ |  | | | | (_| | | | | | |
+   \_/ \___|_|  |_| |_|\__,_|_| |_| |_|
+
+Enter in your key")
+                        key = ReadLine()
+                    Case 1
+                        key = generateKey(input.Length())
+                        WriteLine($"__     __                              
+\ \   / /__ _ __ _ __   __ _ _ __ ___  
+ \ \ / / _ \ '__| '_ \ / _` | '_ ` _ \ 
+  \ V /  __/ |  | | | | (_| | | | | | |
+   \_/ \___|_|  |_| |_|\__,_|_| |_| |_|
+
+Your key is: {key}")
+                End Select
+            Case -1
+                WriteLine("__     __                              
+\ \   / /__ _ __ _ __   __ _ _ __ ___  
+ \ \ / / _ \ '__| '_ \ / _` | '_ ` _ \ 
+  \ V /  __/ |  | | | | (_| | | | | | |
+   \_/ \___|_|  |_| |_|\__,_|_| |_| |_|
+
+Enter in your key")
+                key = ReadLine()
+        End Select
+
+        output = xorText(input, key)
+
+        Return output
+    End Function
+
+    Function generateKey(length As Integer) As String
+        Dim key As String = ""
+        For i = 0 To length - 1
+            Randomize()
+            key &= Chr((Rnd() * 30) + 33)
+        Next i
+
+        Return key
+    End Function
+
+    Function xorText(plain As String, key As String)
+        Dim encrypt As String = ""
+        Dim plainCurBin(7) As Integer
+        Dim keyCurBin(7) As Integer
+        Dim encryptCurBin(7) As Integer
+
+        For i = 0 To plain.Length() - 1
+            If plain(i) = " " Then
+                encrypt &= " "
+            Else
+                plainCurBin = decToBin(Asc(plain(i)))
+                keyCurBin = decToBin(Asc(key(i)))
+
+                For j = 0 To 7
+                    encryptCurBin(j) = CInt(CBool(plainCurBin(j)) Xor CBool(keyCurBin(j))) * -1
+                Next j
+
+                encrypt &= Chr(binToDec(encryptCurBin))
+            End If
+
+        Next i
+
+        Return encrypt
+    End Function
+
+    Function decToBin(dec As Integer) As Integer()
+        Dim convertedNum(7) As Integer
+        For i = 7 To 0 Step -1
+            convertedNum(i) = dec Mod 2
+            dec = dec \ 2
+        Next i
+
+        Return convertedNum
+    End Function
+
+    Function binToDec(bin As Integer()) As Integer
+        Dim dec As Integer = 0
+        For i = 0 To 7
+            dec += 2 ^ (7 - i) * bin(i)
+        Next i
+
+        Return dec
+    End Function
+
+    Function decryptRailFence() As String
         Dim decrypt As String = ""
-        Dim rail1 As String = ""
-        Dim rail2 As String = ""
-        Dim rail3 As String = ""
         Dim add1 As Integer = 0
         Dim add2 As Integer = 0
 
@@ -120,7 +243,7 @@ Please choose an option:
 
 Please enter in your encrypted text")
 
-        input = ReadLine().ToUpper()
+        Dim input As String = ReadLine().ToUpper()
         input = cleanInput(input)
 
         If input.Length() Mod 4 <> 0 Then
@@ -134,9 +257,9 @@ Please enter in your encrypted text")
                 add2 = 2
         End Select
 
-        rail1 = input.Substring(0, (input.Length / 4) + add1)
-        rail2 = input.Substring((input.Length / 4) + add1, (input.Length / 2) + add2)
-        rail3 = input.Substring(((input.Length / 4) * 3) + add2 + add1, input.Length / 4)
+        Dim rail1 As String = input.Substring(0, (input.Length / 4) + add1)
+        Dim rail2 As String = input.Substring((input.Length / 4) + add1, (input.Length / 2) + add2)
+        Dim rail3 As String = input.Substring(((input.Length / 4) * 3) + add2 + add1, input.Length / 4)
 
         For i = 1 To input.Length()
             If i Mod 2 = 0 Then
@@ -151,20 +274,20 @@ Please enter in your encrypted text")
             End If
         Next i
 
-        WriteLine(decrypt)
-    End Sub
+        Return decrypt
+    End Function
 
-    Sub caesar(direction As Integer)
+    Function caesar(direction As Integer) As String
         Dim output As String = ""
-        Dim name As String = "plain"
-        If direction = -1 Then name = "encrypted"
+        Dim type As String = "plain"
+        If direction = -1 Then type = "encrypted"
         WriteLine($"  ____
  / ___|__ _  ___  ___  __ _ _ __
 | |   / _` |/ _ \/ __|/ _` | '__|
 | |__| (_| |  __/\__ \ (_| | |
  \____\__,_|\___||___/\__,_|_|
 
-Enter in your {name} text")
+Enter in your {type} text")
         Dim input As String = ReadLine().ToUpper()
         input = cleanInput(input)
         Dim key As Integer = tryCatch("Enter your key")
@@ -181,15 +304,14 @@ Enter in your {name} text")
             output += Chr(letter)
         Next i
 
-        WriteLine(output)
-    End Sub
+        Return output
+    End Function
 
-    Sub vigenere(direction As Integer)
+    Function vigenere(direction As Integer) As String
         Dim output As String = ""
-        Dim curShift As Integer = 0
-        Dim name As String = "plain"
+        Dim type As String = "plain"
 
-        If direction = -1 Then name = "encrypted"
+        If direction = -1 Then type = "encrypted"
 
         WriteLine($"__     ___
 \ \   / (_) __ _  ___ _ __   ___ _ __ ___
@@ -198,7 +320,7 @@ Enter in your {name} text")
    \_/  |_|\__, |\___|_| |_|\___|_|  \___|
            |___/
 
-Enter in you {name} text:")
+Enter in you {type} text:")
         Dim input As String = ReadLine().ToUpper()
         input = cleanInput(input)
 
@@ -206,7 +328,7 @@ Enter in you {name} text:")
         Dim key As String = ReadLine().ToUpper()
         For i = 0 To input.Length() - 1
             Dim letter As Integer = 0
-            curShift = Asc(key(i Mod key.Length())) - 65
+            Dim curShift As Integer = Asc(key(i Mod key.Length())) - 65
             If Asc(input(i)) < 91 And Asc(input(i)) > 64 Then
                 letter = Asc(input(i)) + (curShift * direction)
                 If letter < 65 Then letter += 26
@@ -217,11 +339,10 @@ Enter in you {name} text:")
             output += Chr(letter)
         Next i
 
-        WriteLine(output)
-    End Sub
+        Return output
+    End Function
 
-    Sub encryptRailFence()
-        Dim input As String = ""
+    Function encryptRailFence() As String
         Dim encrypt As String = ""
         WriteLine(" ____       _ _   _____
 |  _ \ __ _(_) | |  ___|__ _ __   ___ ___
@@ -231,7 +352,7 @@ Enter in you {name} text:")
 
 Please enter in your plain text")
 
-        input = ReadLine().ToUpper()
+        Dim input As String = ReadLine().ToUpper()
         input = cleanInput(input)
 
         For i = 0 To input.Length() - 1 Step 4
@@ -250,8 +371,8 @@ Please enter in your plain text")
             encrypt &= input(i)
         Next i
 
-        WriteLine(encrypt)
-    End Sub
+        Return encrypt
+    End Function
 
     Function cleanInput(input As String) As String
         Dim output As String = ""
@@ -263,9 +384,8 @@ Please enter in your plain text")
 
     Sub Main()
         Dim use As Boolean = True
-        Dim input As Integer = 0
         Do
-            input = userChoice(" __  __       _ _   _       _
+            Dim input As Integer = userChoice(" __  __       _ _   _       _
 |  \/  |_   _| | |_(_)_ __ | | ___
 | |\/| | | | | | __| | '_ \| |/ _ \
 | |  | | |_| | | |_| | |_) | |  __/
